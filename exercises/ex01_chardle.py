01fbb1663b9fb00adf4a725400d7a9a872ffa893
Arden Feldt <feldt@ad.unc.edu>
2023-01-24 16:26:30 -0500

    third try on chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,42 +4,42 @@ __author__ = "730566605"
 
 word: str = input("Enter a 5-character word:")
 
-if(len (word) != 5):
+if (len (word) != 5):
     print("Error: Word must contain 5 characters")
     exit()
 
 char: str = input("Enter a single character:")
 count: int = 0
 
-if(len (char) != 1):
+if (len(char) != 1):
     print("Error: Character must be a single character")
     exit()
 
 print("Searching for " + char + " in " + str(word))
 
-if(char == word [0]):
+if (char == word[0]):
     print(char + " found at index 0")
     count = count + 1
 
-if(char == word [1]):
+if (char == word[1]):
     print(char + " found at index 1")
     count = count + 1
 
-if(char == word [2]):
+if (char == word[2]):
     print(char + " found at index 2")
     count = count + 1
 
-if(char == word [3]):
+if (char == word[3]):
     print(char + " found at index 3")
     count = count + 1
 
-if(char == word [4]):
+if (char == word[4]):
     print(char + " found at index 4")
     count = count + 1
 
 if (count > 1):
-    print(str (count) + " instances of " + char + " found in " + word)
+    print(str(count) + " instances of " + char + " found in " + word)
 elif (count == 1):
-    print(str (count) + " instance of " + char + " found in " + word)
+    print(str(count) + " instance of " + char + " found in " + word)
 else:
     print("No instances of " + char + " found in " + word)
